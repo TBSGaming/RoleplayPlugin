039819f1c9ef7c36f0199c2e095d3d1c13f6b4f1
TBSGaming <schloessi3007yt@gmail.com>
2024-02-23 13:56:19 +0100

    Creation

---
+++ src/main/java/org/example/tbs/roleplayplugin/RoleplayPlugin.java
@@ -0,0 +1,17 @@
+package org.example.tbs.roleplayplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RoleplayPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
